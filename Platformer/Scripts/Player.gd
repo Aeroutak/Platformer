a0a66e439bb0fc208cf61207753e514df3c36348
Aaron Cala <aaronjcala@gmail.com>
2020-08-24 19:43:38 -0600

    Added smooth air movement

--- Platformer/Scripts/Player.gd
+++ Platformer/Scripts/Player.gd
@@ -1,20 +1,28 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
+const GRAVITY = 30
+const MAX_SPEED = 300
+const ACCELERATION = 100
+const JUMP_HEIGHT = -600
 var motion = Vector2()
 
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 	else:
-		motion.x = 0
+		friction = true
+		
 		
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			motion.y = -400
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 		
 	motion = move_and_slide(motion, UP)
